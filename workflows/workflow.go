f0e6feaae0776e96b9ba0d01bb53792e3f871185
Saroj Mahato <hi.creator54@gmail.com>
2025-03-15 23:48:58 +0530

    Update workflow.go

--- workflows/workflow.go
+++ workflows/workflow.go
@@ -4,6 +4,5 @@ import "go.temporal.io/sdk/workflow"
 
 // HelloWorldWorkflow defines the interface for our workflow
 type HelloWorldWorkflow interface {
-	// SayHello matches the Java example's method name
 	SayHello(ctx workflow.Context, name string) (string, error)
 } 
